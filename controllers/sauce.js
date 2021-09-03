64fdd75a76fae542853989d0897e0fb95c093627
Alexis Fillion <fillion.alexis@gmail.com>
2021-09-03 12:35:36 +0200

    add verify_user function to delOne sauce's controllers

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -60,12 +60,25 @@ exports.modifOne = (req, res, next) => {
 }
 
 exports.delOne = (req, res, next) => {
+  console.log(req.headers);
+  console.log(req.params);
+  console.log(req.body);
+  Sauce.findById({ _id: req.params.id })
+  .then((sauce) => {
+    if (!verify_user(req.headers.authorization.split(' ')[1], sauce.userId)) {
+      res.status(403).json({message: "Unauthorized"});
+      return;
+    }
+  })
+  .catch((error) => {
+    res.status(404).json({error});
+  });
   Sauce.deleteOne({ _id: req.params.id })
   .then(() => {
-    res.status(200).json({ message: 'Objet supprimé !'})
+    res.status(200).json({ message: 'Objet supprimé !'});
   })
   .catch(error => {
-    res.status(400).json({ error })
+    res.status(400).json({ error });
   });
 }
 
